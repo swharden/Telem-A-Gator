0daef869028184586bd05846a446ed8d604d90b7
Scott W Harden <swharden@gmail.com>
2017-10-30 18:10:38 -0400

    upload screenshots

--- src/LAUNCH.cmd
+++ src/LAUNCH.cmd
@@ -1,2 +1,2 @@
-"C:\Users\swharden\Documents\important\WinPython-64bit-2.7.10.3\python-2.7.10.amd64\python.exe" TELEM-A-GATOR.py
+"C:\Users\swharden\Documents\important\WinPython-64bit-2.7.12.4Zero\python-2.7.12.amd64\python.exe" TELEM-A-GATOR.py
 pause
